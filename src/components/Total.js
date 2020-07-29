99a1aa62540cd64d112a0dc4fe99f95d4d801b1b
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-07-29 08:40:01 -0400

    Update Total.js

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+//import { useSelector } from 'react-redux';
+import { connect } from "react-redux";
 
 const Total = props => {
 
-  const price = useSelector(state => state.car.price)
+  //const price = useSelector(state => state.car.price)
 
   return (
 
     <div className="content">
 
-      <h4>Total Amount: ${price}</h4>
+      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
 
     </div>
 
@@ -17,4 +18,9 @@ const Total = props => {
   
 };
 
-export default Total;
+//export default Total;
+
+export default connect(state => ({
+  car: state.car,
+  additionalPrice: state.additionalPrice
+}))(Total);
