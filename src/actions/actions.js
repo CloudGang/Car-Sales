0e9fbb3f78e5fdd902545f3315c39a8439aa5e40
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-07-29 08:40:15 -0400

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,24 +1,24 @@
 export const ADD_FEATURE = 'ADD_FEATURE';
 export const DELETE_FEATURE = 'DELETE_FEATURE';
 
-export function addFeature(item) {
+export const addFeature = (item) => ({
 
-  return {
+
 
     type: ADD_FEATURE,
     payload: item
 
-  }
 
-}
 
-export function deleteFeature(item) {
+});
 
-  return {
+export const deleteFeature = (item) => ({
+
+
 
     type: DELETE_FEATURE,
     payload: item
 
-  }
+
   
-}
+});
